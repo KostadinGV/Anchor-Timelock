7e6ef045d9ca6c205a3643ba49c7514a130f0419
root <root@65-108-126-114.serverhub.ru>
2022-02-21 12:41:39 +0100

    Complete test

--- programs/first/src/lib.rs
+++ programs/first/src/lib.rs
@@ -1,115 +1,176 @@
 
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, TokenAccount, Transfer};
-use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{
-    program_error::ProgramError,
-};
-declare_id!("56n2n8MBEqqSNMEsTmuk2RrRCAswhGvmLNaeUMg82dTN");
+use anchor_spl::token::{self, Token, TokenAccount, SetAuthority, Mint, Transfer};
+use spl_token::instruction::AuthorityType;
+declare_id!("GQ9tD6hwnaHvSvwyFEEwKxHAhy3thvhiVFTE33LsKeye");
 
+const UNLOCK_PERIOD : u64 = 0;
 #[program]
 pub mod first {
+
     use super::*;
+
+    const VAULT_AUTHORITY_SEED: &[u8] = b"vault-authority";
+
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        _vault_account_bump: u8
+    ) -> ProgramResult {
+        let (vault_authority, _vault_authority_bump) = 
+            Pubkey::find_program_address(&[VAULT_AUTHORITY_SEED], ctx.program_id);
+        token::set_authority(
+            ctx.accounts.into_set_authority_context(),
+            AuthorityType::AccountOwner,
+            Some(vault_authority)
+        )?;
+        Ok(())
+    }
 
     pub fn deposit(
         ctx: Context<Deposit>,
+        _staker_account_bump: u8,
         amount: u64
     ) -> ProgramResult {
+        let staker = &mut ctx.accounts.token_staker;
+        staker.staked_amount += amount;
+        staker.owner = *ctx.accounts.owner.key;
+        staker.unlock_time = Clock::get()?.unix_timestamp as u64 + UNLOCK_PERIOD;
+
         token::transfer(
             ctx.accounts.into_token_transfer_context(),
             amount
         )?;
-        let mut staker: TokenStaker = TokenStaker::deserialize(&mut &ctx.accounts.token_staker.data.borrow()[..])?;
-        staker.staked_amount += amount;
-        staker.owner = *ctx.accounts.owner.key;
-        staker.serialize(&mut &mut ctx.accounts.token_staker.data.borrow_mut()[..])?;
         Ok(())
     }
 
     pub fn withdraw(
         ctx: Context<Withdraw>,
+        amount: u64
     ) -> ProgramResult {
-        let mut staker: TokenStaker = TokenStaker::deserialize(&mut &ctx.accounts.token_staker.data.borrow()[..])?;
-        if staker.owner != *ctx.accounts.owner.key {
-            msg!("owner mismatch");
-            return Err(ProgramError::InvalidAccountData);
+        let staker = &mut ctx.accounts.token_staker;
+        let cur_time = Clock::get()?.unix_timestamp as u64;
+
+        if cur_time < staker.unlock_time {
+            return Err(CustomError::Locked.into());
         }
+
+        if amount > staker.staked_amount {
+            return Err(CustomError::ExceedAmount.into());
+        }
+
+        staker.staked_amount -= amount;
+
+        let (_vault_authority, vault_authority_bump) =
+            Pubkey::find_program_address(&[VAULT_AUTHORITY_SEED], ctx.program_id);
+        let authority_seeds = &[&VAULT_AUTHORITY_SEED[..], &[vault_authority_bump]];
+
         token::transfer(
-            ctx.accounts.into_token_transfer_context(),
-            staker.staked_amount
+            ctx.accounts
+                .into_token_transfer_context()
+                .with_signer(&[&authority_seeds[..]]),
+            amount
         )?;
-        staker.staked_amount = 0;
-        staker.serialize(&mut &mut ctx.accounts.token_staker.data.borrow_mut()[..])?;
+        // with_signer []
         Ok(())
     }
 }
 
-
 #[derive(Accounts)]
+#[instruction(_vault_account_bump: u8)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    #[account(
+        init,
+        seeds = [b"vault".as_ref(),owner.key.as_ref()],
+        bump = _vault_account_bump,
+        payer = owner,
+        token::mint = source_token_mint,
+        token::authority = owner,
+    )]
+    pub vault_account: Account<'info, TokenAccount>,
+    pub source_token_mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>
+}
+#[derive(Accounts)]
+#[instruction(_staker_account_bump: u8)]
 pub struct Deposit<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
 
-    #[account(mut)]
-    pub token_staker: AccountInfo<'info>,
+    #[account(
+        init,
+        seeds = [b"staker".as_ref(), owner.key.as_ref()],
+        bump = _staker_account_bump,
+        payer = owner
+    )]
+    pub token_staker: Account<'info, TokenStaker>,
 
     #[account(
         mut,
-        constraint = source_token_account.mint == *source_token_mint.key
+        constraint = source_token_account.mint == source_token_mint.key()
     )]
     pub source_token_account: Account<'info, TokenAccount>,
-
-    #[account(mut, owner = spl_token::id())]
-    pub source_token_mint: AccountInfo<'info>,
+    pub source_token_mint: Account<'info, Mint>,
 
     #[account(
         mut,
-        constraint = token_vault_account.mint == *source_token_mint.key
+        constraint = token_vault_account.mint == source_token_mint.key()
     )]
     pub token_vault_account: Account<'info, TokenAccount>,   
 
-    #[account(address = spl_token::id())]
-    pub token_program : AccountInfo<'info>,
+    pub token_program : Program<'info, Token>,
+    pub system_program : Program<'info, System>
 }
 
 #[derive(Accounts)]
 pub struct Withdraw<'info> {
-
-    #[account(signer)]
-    pub admin: AccountInfo<'info>,
-
     #[account(signer)]
     pub owner: AccountInfo<'info>,
 
-    #[account(mut)]
-    pub token_staker: AccountInfo<'info>,
+    #[account(
+        mut,
+        has_one = owner
+    )]
+    pub token_staker: Account<'info, TokenStaker>,
 
     #[account(
         mut,
-        constraint = dest_token_account.mint == *token_mint.key,
+        constraint = dest_token_account.mint == token_mint.key(),
         constraint = dest_token_account.owner == *owner.key
     )]
     pub dest_token_account: Account<'info, TokenAccount>,
 
-    #[account(owner = spl_token::id())]
-    pub token_mint: AccountInfo<'info>,
+    pub token_mint: Account<'info, Mint>,
 
     #[account(
         mut,
-        constraint = token_vault_account.mint == *token_mint.key
+        constraint = token_vault_account.mint == token_mint.key()
     )]
     pub token_vault_account: Account<'info, TokenAccount>, 
-
-    #[account(address = spl_token::id())]
-    pub token_program : AccountInfo<'info>,
+    pub token_program : Program<'info, Token>,
+    pub vault_authority: AccountInfo<'info>
 }
 
-#[derive(BorshDeserialize, BorshSerialize, Debug)]
+#[account]
+#[derive(Default)]
 pub struct TokenStaker {
     pub owner: Pubkey,
-    pub staked_amount: u64
+    pub staked_amount: u64,
+    pub unlock_time: u64
 }
-
+impl<'info> Initialize<'info> {
+    fn into_set_authority_context(&self) -> CpiContext<'_,'_,'_, 'info, SetAuthority<'info>> {
+        let cpi_accounts = SetAuthority {
+            account_or_mint: self.vault_account.to_account_info().clone(),
+            current_authority: self.owner.to_account_info().clone(),
+        };
+        CpiContext::new(self.token_program.to_account_info().clone(), cpi_accounts)
+    }
+}
 impl<'info> Deposit<'info> {
     fn into_token_transfer_context(
         &self
@@ -119,7 +180,7 @@ impl<'info> Deposit<'info> {
             to: self.token_vault_account.to_account_info().clone(),
             authority: self.owner.to_account_info().clone(),
         };
-        CpiContext::new(self.token_program.clone(), cpi_accounts)
+        CpiContext::new(self.token_program.to_account_info().clone(), cpi_accounts)
     }
 }
 impl<'info> Withdraw<'info> {
@@ -129,9 +190,17 @@ impl<'info> Withdraw<'info> {
         let cpi_accounts = Transfer {
             from: self.token_vault_account.to_account_info().clone(),
             to: self.dest_token_account.to_account_info().clone(),
-            authority: self.admin.clone(),
+            authority: self.vault_authority.clone(),
         };
-        CpiContext::new(self.token_program.clone(), cpi_accounts)
+        CpiContext::new(self.token_program.to_account_info().clone(), cpi_accounts)
     }
 }
 
+#[error]
+pub enum CustomError {
+    #[msg("Token still locked!")]
+    Locked,
+    #[msg("Token amount exceed!")]
+    ExceedAmount
+
+}
---
+++ tests/test.ts
@@ -0,0 +1,205 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { First } from '../target/types/first';
+
+import { 
+  PublicKey, 
+  SystemProgram, 
+  Transaction, 
+} from '@solana/web3.js';
+
+import { TOKEN_PROGRAM_ID, Token, AccountLayout} from "@solana/spl-token";
+import { assert } from "chai";
+
+describe('test', () => {
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const firstProgram = anchor.workspace.First as Program<First>;
+
+  let tokenMint = null;
+
+  let userTokenAccount = null;
+  let user1TokenAccount = null;
+
+  const admin = anchor.web3.Keypair.generate();
+  const user = anchor.web3.Keypair.generate();
+  const user1 = anchor.web3.Keypair.generate();
+
+  const tokenSupply = 1000000000000;
+
+  const userTokenAmount = 1000;
+  const user1TokenAmount = 2000;
+
+  const userWithdrawAmount = 500;
+
+  it('Is initialized!', async () => {
+    // Add your test here.
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(admin.publicKey, 1000000000),
+      "confirmed"
+    );
+    await provider.send(
+      (() => {
+        const tx = new Transaction();
+        tx.add(
+          SystemProgram.transfer({
+            fromPubkey: admin.publicKey,
+            toPubkey: user.publicKey,
+            lamports: 100000000,
+          })
+        );
+        return tx;
+      })(),
+      [admin]
+    );
+    await provider.send(
+      (() => {
+        const tx = new Transaction();
+        tx.add(
+          SystemProgram.transfer({
+            fromPubkey: admin.publicKey,
+            toPubkey: user1.publicKey,
+            lamports: 100000000,
+          })
+        );
+        return tx;
+      })(),
+      [admin]
+    );
+
+    tokenMint = await Token.createMint(
+      provider.connection,
+      admin,
+      admin.publicKey,
+      null,
+      0,
+      TOKEN_PROGRAM_ID
+    );
+    
+    userTokenAccount = await tokenMint.createAccount(user.publicKey);
+    user1TokenAccount = await tokenMint.createAccount(user1.publicKey);
+    await tokenMint.mintTo(
+      userTokenAccount,
+      admin.publicKey,
+      [admin],
+      userTokenAmount
+    );
+    await tokenMint.mintTo(
+      user1TokenAccount,
+      admin.publicKey,
+      [admin],
+      user1TokenAmount
+    );
+    let userTokenInfo = await tokenMint.getAccountInfo(userTokenAccount);
+    let user1TokenInfo = await tokenMint.getAccountInfo(user1TokenAccount);
+
+    assert.ok(userTokenInfo.amount.toNumber() == userTokenAmount);
+    assert.ok(user1TokenInfo.amount.toNumber() == user1TokenAmount);
+
+    
+  });
+  it("Initialize", async() => {
+    const [_vault_pda, _vault_pda_bump] = await PublicKey.findProgramAddress(
+      [Buffer.from(("vault")), admin.publicKey.toBuffer()],
+      firstProgram.programId
+    );
+    let txHash = await firstProgram.rpc.initialize(
+      _vault_pda_bump,
+      {
+        accounts: {
+          owner: admin.publicKey,
+          sourceTokenMint: tokenMint.publicKey,
+          vaultAccount: _vault_pda,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY
+        },
+        signers: [admin],
+      }
+    );
+    console.log("txHash =", txHash);
+  });
+  it("Deposit", async () => {
+    const [_user_staker_pda, _user_staker_pda_bump] = await PublicKey.findProgramAddress(
+      [Buffer.from("staker"), user.publicKey.toBuffer()],
+      firstProgram.programId
+    );
+    const [_user1_staker_pda, _user1_staker_pda_bump] = await PublicKey.findProgramAddress(
+      [Buffer.from("staker"), user1.publicKey.toBuffer()],
+      firstProgram.programId
+    );
+    const [_vault_pda, _vault_pda_bump] = await PublicKey.findProgramAddress(
+      [Buffer.from("vault"), admin.publicKey.toBuffer()],
+      firstProgram.programId
+    );
+        
+    await firstProgram.rpc.deposit(
+      _user_staker_pda_bump,
+      new anchor.BN(userTokenAmount),
+      {
+        accounts: {
+          owner: user.publicKey,
+          sourceTokenAccount: userTokenAccount,
+          sourceTokenMint: tokenMint.publicKey,
+          tokenVaultAccount: _vault_pda,
+          tokenStaker: _user_staker_pda,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          systemProgram: anchor.web3.SystemProgram.programId
+        },
+        signers: [user]
+    });
+    await firstProgram.rpc.deposit(
+      _user1_staker_pda_bump,
+      new anchor.BN(user1TokenAmount),
+      {
+        accounts: {
+          owner: user1.publicKey,
+          sourceTokenAccount: user1TokenAccount,
+          sourceTokenMint: tokenMint.publicKey,
+          tokenVaultAccount: _vault_pda,
+          tokenStaker: _user1_staker_pda,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          systemProgram: anchor.web3.SystemProgram.programId
+        },
+        signers: [user1]
+    });
+  });
+    
+  it("Withdraw", async () => {
+    const [_vault_pda, _vault_pda_bump] = await PublicKey.findProgramAddress(
+      [Buffer.from("vault"), admin.publicKey.toBuffer()],
+      firstProgram.programId
+    );
+    const [_user_staker_pda, _user_staker_pda_bump] = await PublicKey.findProgramAddress(
+      [Buffer.from("staker"), user.publicKey.toBuffer()],
+      firstProgram.programId
+    );
+    const [_vault_authority_pda, _vault_authority_pda_bump] = await PublicKey.findProgramAddress(
+      [Buffer.from("vault-authority")],
+      firstProgram.programId
+    );
+    await firstProgram.rpc.withdraw(
+      new anchor.BN(userWithdrawAmount),
+      {
+        accounts: {
+          owner: user.publicKey,
+          destTokenAccount: userTokenAccount,
+          tokenMint: tokenMint.publicKey,
+          tokenVaultAccount: _vault_pda,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          tokenStaker: _user_staker_pda,
+          vaultAuthority: _vault_authority_pda,
+        },
+        signers: [user]
+      }
+    );
+
+    let _userTokenAccount = await tokenMint.getAccountInfo(userTokenAccount);
+    let _user1TokenAccount = await tokenMint.getAccountInfo(user1TokenAccount);
+    console.log(_userTokenAccount.amount.toNumber());
+    console.log(_user1TokenAccount.amount.toNumber());
+    assert.ok(_userTokenAccount.amount.toNumber() == userWithdrawAmount);
+    assert.ok(_user1TokenAccount.amount.toNumber() == 0);
+  });
+});
